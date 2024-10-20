9ab9d78a3a327c24add6632f9d60e42f973990a1
Terry <terryluan12@gmail.com>
2024-10-19 22:35:54 -0400

    * reorganized DoganBoard + Deleted BoardInterface class due to inability to override stl containers filled with parent classes + lack of common elements with other potential game boards

--- dogan/libs/DoganBoard.cpp
+++ dogan/libs/DoganBoard.cpp
@@ -31,6 +31,84 @@ DoganBoard::DoganBoard(DoganConfig config) {
   cells.at(config.getRobberLocation())->setRobber(true);
 }
 
+size_t DoganBoard::getBoardSize(void) const { return boardSize; }
+
+DoganBuilding DoganBoard::getBuilding(Coordinate2D c, Direction d) const {
+  DoganVertex dv(c, d);
+  const auto &map = buildings.find(c);
+  if (map == buildings.end() || map->second.find(d) == map->second.end()) {
+    throw NoSuchStructureException("Error: No Building at given location");
+  }
+  return *(map->second.at(d));
+}
+
+const std::vector<DoganPort> DoganBoard::getPorts(void) const { return ports; }
+
+Coordinate2D DoganBoard::getRobberLocation(void) const {
+  return robberLocation;
+}
+
+std::map<int, std::array<size_t, 5>>
+DoganBoard::getResourceDistribution(int numberRolled) const {
+  std::map<int, std::array<size_t, 5>> playerDistribution;
+  for (auto &cell : this->numbers.at(numberRolled)) {
+    if (this->buildings.find(cell->getCoordinate()) == this->buildings.end()) {
+      continue;
+    }
+    for (auto &[direction, building] :
+         this->buildings.at(cell->getCoordinate())) {
+      if (cell->getCoordinate() == robberLocation) {
+        continue;
+      }
+      int pid = building->getPlayerID();
+      if (playerDistribution.find(pid) == playerDistribution.end()) {
+        std::array<size_t, 5> resources{0, 0, 0, 0, 0};
+        playerDistribution.emplace(pid, resources);
+      }
+      playerDistribution.at(pid)[static_cast<int>(cell->getResource())] +=
+          building->getWorth();
+    }
+  }
+  return playerDistribution;
+}
+
+bool DoganBoard::hasBuilding(const Coordinate2D c, const Direction d) const {
+  DoganVertex dv(c, d);
+  const auto &map = buildings.find(c);
+  if (map == buildings.end()) {
+    return false;
+  }
+  return map->second.find(d) != map->second.end();
+}
+
+bool DoganBoard::hasStructure(const Coordinate2D c, const Direction d,
+                              StructureType st) const {
+  if (st == StructureType::ROAD) {
+    DoganEdge de(c, d);
+    const auto &map = roads.find(c);
+    if (map == roads.end()) {
+      return false;
+    }
+    return map->second.find(d) != map->second.end();
+
+  } else {
+    DoganVertex dv(c, d);
+    const auto &map = buildings.find(c);
+    if (map == buildings.end()) {
+      return false;
+    }
+    const auto &building = map->second.find(d);
+    if (building == map->second.end()) {
+      return false;
+    }
+    return building->second->getStructureType() == st;
+  }
+}
+
+bool DoganBoard::hasTile(const Coordinate2D c) const {
+  return cells.find(c) != cells.end();
+}
+
 void DoganBoard::buildStructure(std::shared_ptr<DoganStructure> ds,
                                 std::array<int, 5> c) {
   DoganGraphElement dg = *ds->getGraphElements()[0];
@@ -91,98 +169,10 @@ void DoganBoard::buildStructure(std::shared_ptr<DoganStructure> ds,
   }
 }
 
-size_t DoganBoard::getBoardSize(void) const { return boardSize; }
-
-const std::vector<DoganPort> DoganBoard::getPorts(void) const { return ports; }
-
-Coordinate2D DoganBoard::getRobberLocation(void) const {
-  return robberLocation;
-}
-
-DoganBuilding DoganBoard::getBuilding(Coordinate2D c, Direction d) const {
-  DoganVertex dv(c, d);
-  const auto &map = buildings.find(c);
-  if (map == buildings.end() || map->second.find(d) == map->second.end()) {
-    throw NoSuchStructureException("Error: No Building at given location");
-  }
-  return *(map->second.at(d));
-}
-
 void DoganBoard::moveRobber(Coordinate2D nl) {
   cells.at(robberLocation)->setRobber(false);
   cells.at(nl)->setRobber(true);
   robberLocation = nl;
-}
-
-bool DoganBoard::hasStructure(const Coordinate2D c, const Direction d,
-                              StructureType st) const {
-  if (st == StructureType::ROAD) {
-    DoganEdge de(c, d);
-    const auto &map = roads.find(c);
-    if (map == roads.end()) {
-      return false;
-    }
-    return map->second.find(d) != map->second.end();
-
-  } else {
-    DoganVertex dv(c, d);
-    const auto &map = buildings.find(c);
-    if (map == buildings.end()) {
-      return false;
-    }
-    const auto &building = map->second.find(d);
-    if (building == map->second.end()) {
-      return false;
-    }
-    return building->second->getStructureType() == st;
-  }
-}
-
-bool DoganBoard::hasBuilding(const Coordinate2D c, const Direction d) const {
-  DoganVertex dv(c, d);
-  const auto &map = buildings.find(c);
-  if (map == buildings.end()) {
-    return false;
-  }
-  return map->second.find(d) != map->second.end();
-}
-
-bool DoganBoard::hasTile(const Coordinate2D c) const {
-  return cells.find(c) != cells.end();
-}
-
-void DoganBoard::setBoardSize(size_t bs) {
-  if (bs > MAX_BOARD_SIZE) {
-    throw std::out_of_range("Error, attempting to set Board Size to greater "
-                            "than limit. Must be less than " +
-                            MAX_BOARD_SIZE);
-  } else {
-    boardSize = bs;
-  }
-}
-
-std::map<int, std::array<size_t, 5>>
-DoganBoard::getResourceDistribution(int numberRolled) {
-  std::map<int, std::array<size_t, 5>> playerDistribution;
-  for (auto &cell : this->numbers.at(numberRolled)) {
-    if (this->buildings.find(cell->getCoordinate()) == this->buildings.end()) {
-      continue;
-    }
-    for (auto &[direction, building] :
-         this->buildings.at(cell->getCoordinate())) {
-      if (cell->getCoordinate() == robberLocation) {
-        continue;
-      }
-      int pid = building->getPlayerID();
-      if (playerDistribution.find(pid) == playerDistribution.end()) {
-        std::array<size_t, 5> resources{0, 0, 0, 0, 0};
-        playerDistribution.emplace(pid, resources);
-      }
-      playerDistribution.at(pid)[static_cast<int>(cell->getResource())] +=
-          building->getWorth();
-    }
-  }
-  return playerDistribution;
 }
 
 std::ostream &operator<<(std::ostream &os, DoganBoard const &db) {
--- dogan/libs/DoganBoard.h
+++ dogan/libs/DoganBoard.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "BoardInterface.h"
 #include "Coordinate.h"
 #include "DoganBank.h"
 #include "DoganBuilding.h"
@@ -11,25 +10,27 @@
 #include <memory>
 #include <random>
 
-class DoganBoard : public BoardInterface<100> {
+class DoganBoard {
 public:
   DoganBoard(DoganConfig config);
-  ~DoganBoard(void){};
-  void buildStructure(std::shared_ptr<DoganStructure> ds, std::array<int, 5> c);
-  size_t getBoardSize(void) const override;
+
+  // Getters
+  size_t getBoardSize(void) const;
+  DoganBuilding getBuilding(Coordinate2D c, Direction d) const;
   const std::vector<DoganPort> getPorts(void) const;
   Coordinate2D getRobberLocation(void) const;
-  DoganBuilding getBuilding(Coordinate2D c, Direction d) const;
-  void moveRobber(Coordinate2D nl);
-  bool hasTile(const Coordinate2D c) const;
+  std::map<int, std::array<size_t, 5>>
+  getResourceDistribution(int numberRolled) const;
+
+  // Hassers
+  bool hasBuilding(const Coordinate2D c, const Direction d) const;
   bool hasStructure(const Coordinate2D c, const Direction d,
                     StructureType st) const;
-  bool hasBuilding(const Coordinate2D c, const Direction d) const;
+  bool hasTile(const Coordinate2D c) const;
 
-  void setBoardSize(size_t bs) override;
-
-  std::map<int, std::array<size_t, 5>>
-  getResourceDistribution(int numberRolled);
+  // Game moves
+  void buildStructure(std::shared_ptr<DoganStructure> ds, std::array<int, 5> c);
+  void moveRobber(Coordinate2D nl);
 
   friend std::ostream &operator<<(std::ostream &os, DoganBoard const &db);
 
